8623c921aa4dc1d9377aabfc896239c09a59b846
YOUR NAME <latori.miller@gmail.com>
2016-03-17 21:02:55 -0400

    adds gitignore

--- modules/App.js
+++ modules/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Link from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return <div>
+              <h1>React Router Tutorial</h1>
+              <ul role='nav'>
+                <li>About</li>
+                <li>Repos</li>
+              </ul>
+    Hello Latori, You are using React Router! Get Excited!!!</div>
   }
 })
